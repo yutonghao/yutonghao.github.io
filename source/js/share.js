30e2d891308198b234c9c5d03b053b5d9c873b45
Ezail <1760992334@qq.com>
2023-12-17 23:21:42 +0800

    魔改2

---
+++ source/js/share.js
@@ -0,0 +1,68 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 剪贴板兼容http协议场景
+function copyToClipboard(textToCopy) {
+    // navigator clipboard 需要https等安全上下文
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard 向剪贴板写文本
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // 创建text area
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // 使text area不在viewport，同时设置不可见
+        textArea.style.position = "absolute";
+        textArea.style.opacity = 0;
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // 执行复制命令并移除文本框
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
+}
+
+// 分享本页
+function share_() {
+    let url = window.location.origin + window.location.pathname
+    try {
+        // 截取标题
+        var title = document.title;
+        var subTitle = title.endsWith("| Ezailkmth") ? title.substring(0, title.length - 14) : title;
+        // navigator.clipboard.writeText('Ezailkmth的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！');
+        copyToClipboard('Ezailkmth的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！');
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "成功复制分享信息🎉",
+                    message: "您现在可以通过粘贴直接跟小伙伴分享了！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success", 
+                    duration: 5000
+                });
+                // return { visible: false }
+            }
+        })
+    } catch (err) {
+        console.error('复制失败！', err);
+    }
+    // new ClipboardJS(".share", { text: function () { return '标题：' + document.title + '\n链接：' + url } });
+    // btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
+
+// 防抖
+function share() {
+    debounce(share_, 300);
+}
